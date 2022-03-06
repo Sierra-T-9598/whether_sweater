e7f074aa92af2ebb24dc4e519682d20b976af86b
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-05 17:33:32 -0700

    Update: Add units argument to `::get_forecast`

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,7 @@
 class ForecastFacade
-  def self.get_forecast(location)
+  def self.get_forecast(location, units = 'imperial')
     coordinates = CoordinatesFacade.get_coordinates(location)
     forecast = WeatherService.forecast_data(coordinates.lat, coordinates.lng)
     Forecast.new(forecast)
   end
-end 
+end
