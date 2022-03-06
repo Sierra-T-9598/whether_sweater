ec26d2b55c98aad66b773d5ce206f0ae38e9a49d
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-05 21:30:31 -0700

    Test: update image service

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe ImageService do
     describe '::background_image' do
       it 'returns the json image data for the requested image', :vcr do
         response = ImageService.background_image('Bozeman,MT')
-        
+
         expect(response).to be_a Hash
 
         expect(response[:results][0]).to have_key :urls
         expect(response[:results][0][:urls]).to be_a Hash
 
-        expect(response[:results][0]).to have_key :description
-        expect(response[:results][0][:description]).to be_a String
+        expect(response[:results][0]).to have_key :alt_description
+        expect(response[:results][0][:alt_description]).to be_a String
 
         expect(response[:results][0]).to have_key :user
         expect(response[:results][0][:user]).to be_a Hash
