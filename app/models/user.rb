f9629a8633c7c61932200c9d571a4c0e2d310efe
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-06 17:50:27 -0700

    Update: Update user model and validations to reflect api key use

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates_presence_of :password_digest
   validates_presence_of :email
   validates_uniqueness_of :email, case_sensitive: false
-  validates_presence_of :api_key
+
+  encrypts :private_api_key, key: Lockbox.attribute_key(table: "users", attribute: "private_api_key_ciphertext")
+  blind_index :private_api_key, key: ENV["blind_index_master_key"]
+  before_create :set_private_api_key
+
+  validates_presence_of :private_api_key, allow_blank: true
+  validates_uniqueness_of :private_api_key, allow_blank: true
+
+  private
+
+  def set_private_api_key
+    self.private_api_key = SecureRandom.urlsafe_base64(24) if self.private_api_key.nil?
+  end
 end
