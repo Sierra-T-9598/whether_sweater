93ed6a398418d974cbbdc5e65bbf300a882d8c54
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-07 15:23:30 -0700

    Test: Add `::directions` method

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -22,5 +22,43 @@ RSpec.describe MapService do
         expect(response[:results][0][:locations][0][:latLng][:lng]).to be_a Float
       end
     end
+
+    describe '::directions(from, to)' do
+      describe 'happy path- route between two points' do
+        it 'returns the directions data between the two arguments', :vcr do
+          from = "lyons,CO"
+          to = "bozeman,MT"
+          response = MapService.directions(from, to)
+
+          expect(response).to be_a Hash
+          expect(response).to have_key(:route)
+          expect(response[:route]).to be_a Hash
+
+          expect(response[:route]).to have_key(:routeError)
+          expect(response[:route][:routeError]).to be_a Hash
+
+          expect(response[:route]).to have_key(:formattedTime)
+          expect(response[:route][:formattedTime]).to be_a String
+
+          expect(response[:route]).to have_key(:realTime)
+          expect(response[:route][:realTime]).to be_an Integer
+        end
+      end
+
+      describe 'sad path- impossible route between to points(driving)' do
+        it 'returns a route Error error code euqal to two', :vcr do
+          from = "lyons,CO"
+          to = "london, Enlgand"
+          error_response = MapService.directions(from, to)
+
+          expect(error_response).to be_a Hash
+          expect(error_response).to have_key(:route)
+          expect(error_response[:route]).to be_a Hash
+
+          expect(error_response[:route]).to have_key(:routeError)
+          expect(error_response[:route][:routeError][:errorCode]).to eq(2)
+        end
+      end
+    end
   end
 end
