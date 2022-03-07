ba96edbe151b35f1c6add090f9a2eca4c3661dc6
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-07 11:01:10 -0700

    Feat: Update controller to handle sad path logic and errors

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    book_search = BookSearchFacade.get_books_and_data(params[:location], params[:quantity])
-    render json: BooksSerializer.new(book_search)
+    if params[:quantity].to_i <= 0
+      render json: { error: "Quantity must be greater than 0"}, status: 400
+    else
+      book_search = BookSearchFacade.get_books_and_data(params[:location], params[:quantity])
+      render json: BooksSerializer.new(book_search)
+    end
   end
 end
