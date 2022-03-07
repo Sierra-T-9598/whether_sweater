edd35eab760e1a71339b85994e49bbc5f6d883d1
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-07 09:43:15 -0700

    Test: Add test to get back total number of books returned in search

--- spec/facades/books_facade_spec.rb
+++ spec/facades/books_facade_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe BooksFacade do
     describe '::get_book_search(location)' do
       it 'returns the book data for the desired location', :vcr do
         books = BooksFacade.get_book_search("bozeman")
-      
+
         books.each do |book|
           expect(book).to be_a Book
+        end
+      end
+
+      describe '::get_total_books(location)' do
+        it 'returns the total number of book results for the given location', :vcr do
+          total = BooksFacade.get_total_books('bozeman')
+
+          expect(total).to eq(80)
         end
       end
     end
