ab3a66c060889f2b2aa40d498a1a9dadc621db79
Sierra Tucker <sierra.tucker121@gmail.com>
2022-03-07 11:08:15 -0700

    Feat: Update controller logic to handle a bad search location

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::BooksController < ApplicationController
   def index
+    book_search = BookSearchFacade.get_books_and_data(params[:location], params[:quantity])
     if params[:quantity].to_i <= 0
       render json: { error: "Quantity must be greater than 0"}, status: 400
+    elsif
+      book_search.total_books_found == 0
+      render json: { error: "It looks like there are no matches for the location you gave, please try again"}, status: 400
     else
-      book_search = BookSearchFacade.get_books_and_data(params[:location], params[:quantity])
       render json: BooksSerializer.new(book_search)
     end
   end
